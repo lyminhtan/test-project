5f201ea41799a2644202d6e00fe4fdc4936cde6e
lyminhtan <lyminhtan@gmail.com>
2022-08-04 16:04:02 +0700

    Commit

--- app/core/settings.py
+++ app/core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles') # BASE_DIR / "staticfiles"
